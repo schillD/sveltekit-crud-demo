466fdfb83b1d4a40675e74c4e8c7077d67e0d95f
SchillD <davis.schill04@gmail.com>
2023-07-22 23:37:03 +0200

    added tailwind and style

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -3,6 +3,10 @@
     export let post: any;
 </script>
 
-<h2>{post.id}</h2>
-<p>{post.content}</p>
-<p>{post.created}</p>
+<div class="border border-gray-200 rounded-md p-2 max-w-xl">
+    <div class="flex flex-col justify-between items-center p-8 space-y-4">
+        <h2>{post.id}</h2>
+        <p class="break-all">{post.content}</p>
+        <p>{post.created}</p>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,16 @@
     $: allPosts = data.allPosts;
 </script>
 
-{#each allPosts as post}
-    <Post {post} />
-{/each}
+<div class="grid gap-4 place-content-center m-4">
+    {#each allPosts as post}
+        <Post {post} />
+    {/each}
 
-<form method="POST" use:enhance>
-    <input type="text" name="content" placeholder="Type content here!" />
-    <button type="submit" formaction="?/createPost">Create Post</button>
-    <button type="submit" formaction="?/deleteAllPosts"
-        >Delete Everything</button
-    >
-</form>
+    <form method="POST" use:enhance>
+        <input class="w-full bg-slate-900 border p-4" type="text" name="content" placeholder="Type content here!" />
+        <div class="flex justify-center">
+            <button class="p-4 m-4 border border-gray-200 rounded-md max-w-xl" type="submit" formaction="?/createPost">Create Post</button>
+            <button class="p-4 m-4 border border-gray-200 rounded-md max-w-xl" formaction="?/deleteAllPosts">Delete Everything</button>
+        </div>
+    </form>
+</div>
