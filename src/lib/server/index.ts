1da61c4d459b239ef8ae308e7503fc42bcff455c
SchillD <davis.schill04@gmail.com>
2023-07-22 22:03:40 +0200

    final fix

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,7 +1,7 @@
 import type { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import { DB_PATH } from "$env/static/private";
+import { DB_PATH } from '$env/static/private';
 
 const sqlite = new Database(DB_PATH, { verbose: console.log });
 export const db: BetterSQLite3Database = drizzle(sqlite);
