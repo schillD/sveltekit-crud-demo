459e3a4ad6a667b3201f4796bf6ca838c7c93e6e
SchillD <davis.schill04@gmail.com>
2023-07-24 21:43:29 +0200

    added spam protection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,26 @@
     import { enhance } from "$app/forms";
     import Post from "$lib/Post.svelte";
     import { ip } from "$lib/stores";
+    import { afterUpdate } from "svelte";
     export let data;
     $: allPosts = data.allPosts;
-    
-
+    $: if (allPosts) pressed = false;
+    let pressed = false;
 
     const fetchAndUpdateData = async () => {
-        if($ip === undefined){
+        if ($ip === undefined) {
             return;
         }
-      const res = await fetch($ip +"api/getAllPosts");
-      const data = await res.json();
-      allPosts = data;
-}
+        const res = await fetch($ip + "api/getAllPosts");
+        const data = await res.json();
+        allPosts = data;
+    };
 
-    
     const polling = setInterval(fetchAndUpdateData, 2000);
 
-    
+    function press() {
+        pressed = true;
+    }
 </script>
 
 <div class="grid gap-4 place-content-center m-4">
@@ -27,12 +29,26 @@
         <Post {post} />
     {/each}
 
-    <form method="POST" use:enhance>
-        <textarea class="w-full rounded-md bg-slate-900 hover:bg-slate-950 border p-4 resize-y" style="resize: vertical;" type="text" name="content" placeholder="Type content here!" />
+    <form method="POST" use:enhance={press}>
+        <textarea
+            class="w-full rounded-md bg-slate-900 hover:bg-slate-950 border p-4 resize-y"
+            style="resize: vertical;"
+            type="text"
+            name="content"
+            placeholder="Type content here!"
+        />
 
         <div class="flex justify-center">
-            <button class="p-2 m-4 border border-gray-200 hover:bg-slate-950 rounded-md max-w-xl" type="submit" formaction="?/createPost">Create Post</button>
-            <button class="p-2 m-4 border border-gray-200 hover:bg-slate-950 rounded-md max-w-xl" formaction="?/deleteAllPosts">Delete Everything</button>
+            <button
+                class="p-2 m-4 border border-gray-200 hover:bg-slate-950 rounded-md max-w-xl"
+                type="submit"
+                formaction="?/createPost"
+                disabled={pressed}>Creating Post...</button
+            >
+            <button
+                class="p-2 m-4 border border-gray-200 hover:bg-slate-950 rounded-md max-w-xl"
+                formaction="?/deleteAllPosts">Delete Everything</button
+            >
         </div>
     </form>
 </div>
