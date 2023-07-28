c4b5b3c62e630c8b141dca3aeaeb2bfd350204f8
SchillD <davis.schill04@gmail.com>
2023-07-28 16:00:02 +0200

    enabled wal mode

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -4,4 +4,5 @@ import Database from 'better-sqlite3';
 import { DB_PATH } from '$env/static/private';
 
 const sqlite = new Database(DB_PATH, { verbose: console.log });
+sqlite.pragma('journal_mode = WAL');
 export const db: BetterSQLite3Database = drizzle(sqlite);
